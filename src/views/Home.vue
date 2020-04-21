c7e0fe5990948427a6bc5942978444dc80d5110e
Nurpais Orozulbaev <orozulbaev@gmail.com>
2020-04-21 20:55:10 +0600

    time component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="times-card text-center">
+    <div class="time-card__heading py-4">
+      <h1 class="time-card__title">Бишкек. Время намаза</h1>
+    </div>
+    <div class="time-card__body bg-success text-white rounded-sm">
+      <div class="time-card__calendar">
+        <div class="row mb-6">
+          <div class="col-md-4 text-md-left">cal</div>
+          <div class="col-md-4 text-md-center">cal</div>
+          <div class="col-md-4 text-md-right">cal</div>
+        </div>
+      </div>
+      <div class="time-card__timings">
+        <div class="row">
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="time-card__item">
+              <div class="time-card__time">04:25</div>
+              <div class="time-card__name">Фаджр</div>
+            </div>
+          </div>
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="time-card__item disable-time">
+              <div class="time-card__time">04:25</div>
+              <div class="time-card__name">Фаджр</div>
+            </div>
+          </div>
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="time-card__item">
+              <div class="time-card__time">04:25</div>
+              <div class="time-card__name">Фаджр</div>
+            </div>
+          </div>
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="time-card__item active-time">
+              <div class="time-card__time">04:25</div>
+              <div class="time-card__name">Фаджр</div>
+            </div>
+          </div>
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="time-card__item">
+              <div class="time-card__time">04:25</div>
+              <div class="time-card__name">Фаджр</div>
+            </div>
+          </div>
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="time-card__item">
+              <div class="time-card__time">04:25</div>
+              <div class="time-card__name">Фаджр</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
-</script>
