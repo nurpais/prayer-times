86bea05e46bb60003b9d316b072eee7e04a7b01b
Nurpais <orozulbaev@gmail.com>
2020-04-27 20:58:55 +0600

    css helps

--- src/components/timeCard.vue
+++ src/components/timeCard.vue
@@ -1,64 +1,62 @@
 <template>
-  <div>
-    <div class="card rounded-lg text-center mt-6">
-      <div class="card-header rounded-sm">
-        <h1 class="card-title">Бишкек. Время намазов</h1>
-      </div>
-      <div class="card-body">
-        <!-- Card date -->
-        <div class="card-date" v-if="date.gregorian">
-          <div class="card-date__item">
-            {{ date.gregorian.day }}
-            {{ gregorianMonths[date.gregorian.month.number - 1] }}
-            {{ date.gregorian.year }}
-          </div>
-          <div class="card-date__item">
-            <live-time />
-          </div>
-          <div class="card-date__item">
-            {{ date.hijri.day }}
-            {{ hijriMonths[date.hijri.month.number - 1] }}
-            {{ date.hijri.year }}
-          </div>
+  <div class="card card-success rounded-lg text-center box-shadow mt-6">
+    <div class="card-header rounded-sm">
+      <h1 class="card-title">Бишкек. Время намазов</h1>
+    </div>
+    <div class="card-body">
+      <!-- Card date -->
+      <div class="card-date" v-if="date.gregorian">
+        <div class="card-date__item">
+          {{ date.gregorian.day }}
+          {{ gregorianMonths[date.gregorian.month.number - 1] }}
+          {{ date.gregorian.year }}
         </div>
-        <!-- Card timings -->
-        <div class="card-times" v-if="time.Fajr">
-          <div class="row">
-            <div class="col-md-2 col-sm-4 col-6">
-              <div class="card-times__item">
-                <div class="card-times__time">{{ time.Fajr | rmUTC }}</div>
-                <div class="card-times__name">Фаджр</div>
-              </div>
+        <div class="card-date__item">
+          <live-time />
+        </div>
+        <div class="card-date__item">
+          {{ date.hijri.day }}
+          {{ hijriMonths[date.hijri.month.number - 1] }}
+          {{ date.hijri.year }}
+        </div>
+      </div>
+      <!-- Card timings -->
+      <div class="card-times" v-if="time.Fajr">
+        <div class="row">
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="card-times__item">
+              <div class="card-times__time">{{ time.Fajr | rmUTC }}</div>
+              <div class="card-times__name">Фаджр</div>
             </div>
-            <div class="col-md-2 col-sm-4 col-6">
-              <div class="card-times__item">
-                <div class="card-times__time">{{ time.Sunrise | rmUTC }}</div>
-                <div class="card-times__name">Восход</div>
-              </div>
+          </div>
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="card-times__item">
+              <div class="card-times__time">{{ time.Sunrise | rmUTC }}</div>
+              <div class="card-times__name">Восход</div>
             </div>
-            <div class="col-md-2 col-sm-4 col-6">
-              <div class="card-times__item">
-                <div class="card-times__time">{{ time.Dhuhr | rmUTC }}</div>
-                <div class="card-times__name">Зухр</div>
-              </div>
+          </div>
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="card-times__item">
+              <div class="card-times__time">{{ time.Dhuhr | rmUTC }}</div>
+              <div class="card-times__name">Зухр</div>
             </div>
-            <div class="col-md-2 col-sm-4 col-6">
-              <div class="card-times__item">
-                <div class="card-times__time">{{ time.Asr | rmUTC }}</div>
-                <div class="card-times__name">Аср</div>
-              </div>
+          </div>
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="card-times__item">
+              <div class="card-times__time">{{ time.Asr | rmUTC }}</div>
+              <div class="card-times__name">Аср</div>
             </div>
-            <div class="col-md-2 col-sm-4 col-6">
-              <div class="card-times__item">
-                <div class="card-times__time">{{ time.Maghrib | rmUTC }}</div>
-                <div class="card-times__name">Магриб</div>
-              </div>
+          </div>
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="card-times__item">
+              <div class="card-times__time">{{ time.Maghrib | rmUTC }}</div>
+              <div class="card-times__name">Магриб</div>
             </div>
-            <div class="col-md-2 col-sm-4 col-6">
-              <div class="card-times__item">
-                <div class="card-times__time">{{ time.Isha | rmUTC }}</div>
-                <div class="card-times__name">Иша</div>
-              </div>
+          </div>
+          <div class="col-md-2 col-sm-4 col-6">
+            <div class="card-times__item">
+              <div class="card-times__time">{{ time.Isha | rmUTC }}</div>
+              <div class="card-times__name">Иша</div>
             </div>
           </div>
         </div>
